35b4c128eedb00500b76794ed8621c22a4a82e17
MTLCDevelopers2018 <42overez@gmail.com>
2019-01-08 22:28:59 -0500

    First commit

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define Legion-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "LegionURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
